a129198e09420b1649f397c821debd7383295a47
Nilendu Saha <57375630+nilendusaha23@users.noreply.github.com>
2022-09-14 10:18:07 +0100

    Add files via upload

---
+++ Embedded Systems/multiple_fire_sprinkler/final_webcam_code.py
@@ -0,0 +1,290 @@
+from math import ceil
+import requests
+import cv2
+import serial
+import numpy as np
+
+
+class FireDetection:
+#value initialization label
+    def __init__(self):
+        self.subtractBackground = cv2.createBackgroundSubtractorMOG2(varThreshold=170, detectShadows=False)
+        self.sendSerial = serial.Serial('COM3', 9600)
+        o_kern = ceil(3)
+        c_kern = ceil(30)
+        self.openKernel = np.ones((o_kern, o_kern), np.uint8)
+        self.closeKernel = np.ones((c_kern, c_kern), np.uint8)
+        self.xCentroid = None
+        self.yCentroid = None
+        self.prevRatio = None
+        self.currRatio = None
+        self.prevHist = None
+        self.currHist = None
+        self.prevVar = None
+        self.currVar = None
+        self.history = []
+        self.countFireFrames = 0
+        self.totalFrame = 0
+        self.prevNoOfPixels = 1
+        self.histogram_flag = False
+        self.variance_flag = False
+
+    def volume_analysis(self, threshold_image):
+        gamma = 0.05
+        flag = False
+        no_of_pixels = cv2.compare(threshold_image, 0, cv2.CMP_GT).sum() / 255
+        if self.prevNoOfPixels is not None:
+            if abs(self.prevNoOfPixels - no_of_pixels) / self.prevNoOfPixels > gamma:
+                flag = True
+        self.prevNoOfPixels = no_of_pixels
+        return flag
+
+    def dimension_analysis(self, width, height):
+        gamma = 0.1
+        flag = False
+        self.currRatio = width / height
+        if self.prevRatio is not None:
+            if abs(self.prevRatio - self.currRatio) > gamma:
+                flag = True
+        self.prevRatio = self.currRatio
+        return flag
+
+    def background_subtraction(self, capture_image):
+        mask = self.subtractBackground.apply(capture_image)
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.openKernel)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, self.closeKernel)
+        threshold = cv2.medianBlur(closing, 5)
+        foreground = cv2.bitwise_and(capture_image, cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR))
+        return threshold, foreground
+
+    def color_analysis_org(self, capture_image, threshold_image):
+        div = 240
+        mul = 1.3
+        img_ycrcb = cv2.cvtColor(capture_image, cv2.COLOR_BGR2YCrCb)
+        y, cr, cb = cv2.split(img_ycrcb)
+        y_mean = img_ycrcb[0].mean()*mul
+        cr_mean = cr.mean()
+        cb_mean = cb.mean()
+        cr_mean = cr_mean if cr_mean > 150 else 150
+        cb_mean = 120 if cb_mean > 120 else cb_mean
+        _, y_mat = cv2.threshold(y, div, 255, cv2.THRESH_TOZERO_INV)
+        _, y_mat = cv2.threshold(y_mat, y_mean, 255, cv2.THRESH_TOZERO)
+        y_mat = cv2.compare(y_mat, cb, cv2.CMP_GT)
+        cr_mat = cv2.compare(cr, cb, cv2.CMP_GT)
+        cb_mat = cv2.compare(cb, cb_mean, cv2.CMP_LT)
+        light_fire = cv2.bitwise_and(y_mat, cr_mat)
+        light_fire = cv2.bitwise_and(light_fire, cb_mat)
+        y_mean = y_mean if div < y_mean else div
+        y_mat = cv2.compare(y, y_mean, cv2.CMP_GT)
+        cr_mat = cv2.compare(cr, cb, cv2.CMP_GT)
+        heavy_fire = cv2.bitwise_and(y_mat, cr_mat)
+        fire = cv2.bitwise_or(light_fire, heavy_fire)
+        fire = cv2.bitwise_and(fire, threshold_image)
+        opening = cv2.morphologyEx(fire, cv2.MORPH_OPEN, self.openKernel)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, self.closeKernel)
+        closing = cv2.medianBlur(closing, 5)
+        return_img = cv2.bitwise_and(capture_image, cv2.cvtColor(closing, cv2.COLOR_GRAY2BGR))
+        return return_img
+
+    def fire_blob_detection(self, capture_image, fire_pixel_image, threshold_image):
+        self.totalFrame += 1
+        barrier_frame_limit_hist = 4
+        barrier_frame_limit_var = 2
+        cam_input = capture_image.copy()
+        interval = 10
+        if self.totalFrame % interval == 0:
+            self.alarm_decision(False)
+            self.prevVar = None
+            self.prevHist = None
+            self.xCentroid = None
+        contours, _ = cv2.findContours(cv2.cvtColor(fire_pixel_image, cv2.COLOR_BGR2GRAY),
+                                          cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
+            x_coord, y_coord, width, height = cv2.boundingRect(contours[0])
+
+            if self.centroid_analysis(x_coord, y_coord, width, height):
+                print('centroid true')
+                if self.countFireFrames < barrier_frame_limit_hist:
+                    self.histogram_flag = self.histogram_analysis(capture_image, threshold_image)
+                else:
+                    self.histogram_flag = True
+
+                if self.histogram_flag:
+                    print('histogram true')
+                    if self.countFireFrames < barrier_frame_limit_var:
+                        self.variance_flag = self.variance_analysis(fire_pixel_image[y_coord:y_coord + height,
+                                                               x_coord:x_coord + width])
+                    else:
+                        self.variance_flag = True
+                        
+                    if self.variance_flag:
+                        print('variance true')
+                        cam_input = cv2.rectangle(cam_input,
+                                                  (x_coord, y_coord),
+                                                  ((x_coord + width), (y_coord + height)),
+                                                  (255, 255, 255),
+                                                  2)
+
+                        if self.alarm_decision(True):
+                            print('Fire Alert! {} xFrames / {} Total Frames'.format(self.countFireFrames,
+                                                                                    self.totalFrame))
+                            self.sendSerial.write(str.encode('f'))
+                            cam_input = cv2.rectangle(cam_input,
+                                                      (x_coord, y_coord),
+                                                      ((x_coord + width), (y_coord + height)),
+                                                      (0, 0, 255),
+                                                      2)
+                            x1 = x_coord + width
+                            y1 = y_coord + height
+                            area = height * width
+                            cv2.drawContours(cam_input, contours[0], -1, (0, 0, 255), 2)
+                            print('x = ',x_coord,'y = ',y_coord,'x1 = ',x_coord + width,'y1 = ',y_coord + height,'width = ',width,'ht = ',height,'area = ',area)
+                            if (x_coord < 320 and y_coord < 240 and x_coord + width < 320 and y_coord + height < 240):
+                                self.sendSerial.write(str.encode('a'))
+                                print('Sprinkler 1 Code Red')
+                            elif (x_coord > 320 and y_coord < 240 and x_coord + width > 320 and y_coord + height < 240):
+                                self.sendSerial.write(str.encode('b'))
+                                print('Sprinkler 2 Code White')
+                            elif (x_coord > 320 and y_coord > 240 and x_coord + width > 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('c'))
+                                print('Sprinkler 3 Code Blue')
+                            elif (x_coord < 320 and y_coord > 240 and x_coord + width < 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('d'))
+                                print('Sprinkler 4 Code Yellow')
+                            elif (x_coord < 320 and y_coord < 240 and x_coord + width < 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('e'))
+                                print('Sprinkler 1 & Sprinkler 4 Code Red Yellow')
+                            elif (x_coord < 320 and y_coord < 240 and x_coord + width > 320 and y_coord + height < 240):
+                                self.sendSerial.write(str.encode('f'))
+                                print('Sprinkler 1 & Sprinkler 2 Code Red White')
+                            elif (x_coord > 320 and y_coord < 240 and x_coord + width > 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('g'))
+                                print('Sprinkler 2 & Sprinkler 3 Code White Blue')
+                            elif (x_coord < 320 and y_coord > 240 and x_coord + width > 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('h'))
+                                print('Sprinkler 3 & Sprinkler 4 Code Blue Yellow')
+                            elif (x_coord < 320 and y_coord < 240 and x_coord + width > 320 and y_coord + height > 240):
+                                self.sendSerial.write(str.encode('i'))
+                                print('All Sprinklers Activated')
+                        else:
+                            print('Area Safe')
+                            self.sendSerial.write(str.encode('s'))
+                    else:
+                        print('Area Safe')
+                        self.sendSerial.write(str.encode('s'))
+                else:
+                    print('Area Safe')
+                    self.sendSerial.write(str.encode('s'))
+            else:
+                print('Area Safe')
+                self.sendSerial.write(str.encode('s'))
+        else:
+            print('Area Safe')
+            self.sendSerial.write(str.encode('s'))
+        return cam_input
+
+    def centroid_analysis(self, x_coord, y_coord, width, height):
+        range_limit = 30
+        min_pixel_length = 5
+        flag = False
+        x_center = x_coord + width / 2
+        y_center = y_coord + height / 2
+        if self.xCentroid is not None:
+            if abs(x_center - self.xCentroid) < range_limit:
+                if abs(y_center - self.yCentroid) < range_limit:
+                    if width > min_pixel_length and height > min_pixel_length:
+                        flag = True
+        self.xCentroid = x_center
+        self.yCentroid = y_center
+        print(flag)
+        return flag
+
+    def histogram_analysis(self, capture_image, threshold_image_mask):
+        omega = 0.8
+        flag = False
+        self.currHist = cv2.calcHist([capture_image],
+                                     [0, 1, 2],
+                                     threshold_image_mask,
+                                     [256, 256, 256],
+                                     [0, 150, 0, 150, 0, 150])
+        self.currHist = cv2.normalize(self.currHist, None).flatten()
+        if self.prevHist is not None:
+            correl = cv2.compareHist(self.currHist, self.prevHist, cv2.HISTCMP_CORREL)
+            print(correl)
+            if correl < omega:
+                flag = True
+        self.prevHist = self.currHist
+        return flag
+
+    def variance_analysis(self, image):
+        sigma = 50
+        flag = False
+        ycrcb = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+        y, cr, cb = cv2.split(ycrcb)
+        comp_img = cv2.compare(image, 0, cv2.CMP_GT)
+        ones = comp_img.sum() / 255
+        total = comp_img.size
+        var = np.var(cr)
+        new_var = ((total * var) - ((total - ones) * abs(128-cr.mean()) * abs(128-cr.mean()))) / ones
+        self.currVar = new_var
+        print(new_var)
+        if self.prevVar is not None:
+            if (abs(self.prevVar - self.currVar) > 30) and self.currVar > sigma:
+                flag = True
+        self.prevVar = self.currVar
+        return flag
+
+    def alarm_decision(self, flag):
+        history_storage_limit = 10
+        min_fire_frames = 6
+        alert = False
+        if flag:
+            self.history.append(1)
+        else:
+            self.history.append(0)
+        if sum(self.history) > min_fire_frames:
+            self.countFireFrames += 1
+            alert = True
+        else:
+            self.countFireFrames = 0
+        if len(self.history) > history_storage_limit:
+            self.history = self.history[1:]
+        return alert
+
+#main part
+if __name__ == '__main__':
+    Fire = FireDetection()
+    #url = "http://192.168.0.104:8080/shot.jpg"
+    #cap = cv2.VideoCapture('C:\\Users\\nilen\\OneDrive\\Desktop\\JUproject\\FireDetection1\\test video\\barbeq.avi')
+    cap = cv2.VideoCapture(0)
+    length = 640
+    breadth = 480
+    m = length / 2
+    n = breadth / 2
+    int_m = int(m)
+    int_n = int(n)
+    while True:
+        grabbed, imBGR = cap.read()
+        if not grabbed:
+            break
+        #img_resp = requests.get(url)
+        #img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+        #imBGR = cv2.imdecode(img_arr, -1)
+        factor = 2
+        zone1 = cv2.rectangle(imBGR,(0,0),(int_m,int_n),(0,0,255),3) # Quad 1 red
+        zone2 = cv2.rectangle(imBGR,(0,int_n),(int_m,breadth),(0,255,255),3) # Quad 2 yellow
+        zone3 = cv2.rectangle(imBGR,(int_m,0),(length,int_n),(255,255,255),3) # Quad 3 white
+        zone4 = cv2.rectangle(imBGR,(int_m,int_n),(length,breadth),(255,255,0),3) # Quad 4 blue
+        #imBGR = cv2.resize(imBGR, (ceil(imBGR.shape[1] / factor), ceil(imBGR.shape[0] / factor)))
+        thresholdImage, foregroundImage = Fire.background_subtraction(imBGR)
+        firePixelImage = Fire.color_analysis_org(imBGR, thresholdImage)
+        cv2.imshow('color_segment', firePixelImage)
+        fireBlob = Fire.fire_blob_detection(imBGR, firePixelImage, thresholdImage)
+        cv2.imshow('OUTPUT', fireBlob)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
---
+++ Embedded Systems/multiple_flow_sprinkler/multi_flow_sprinkler/multi_flow_sprinkler/multi_flow_sprinkler.ino
@@ -0,0 +1,135 @@
+int LED=8;
+int Buzzer=9;
+int PumpLine1=2;
+int PumpLine2=3;
+int PumpLine3=4;
+int PumpLine4=5;
+int serialRead=0;
+
+unsigned long pumpCurrMillis = 0;
+unsigned long pumpPrevMillis = 0;
+const long pumpInterval = 3000;
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+const long interval = 200;
+int ledState = LOW; 
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
+  pinMode(PumpLine1, OUTPUT);
+  pinMode(PumpLine2, OUTPUT);
+  pinMode(PumpLine3, OUTPUT);
+  pinMode(PumpLine4, OUTPUT);
+  digitalWrite(PumpLine1, HIGH);
+  //delay(500);
+  digitalWrite(PumpLine2, HIGH);
+  //delay(500);
+  digitalWrite(PumpLine3, HIGH);
+  //delay(500);
+  digitalWrite(PumpLine4, HIGH);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  serialRead=Serial.read();
+  if(serialRead == 'a')
+  {
+    Blink();
+    digitalWrite(PumpLine1, LOW);
+    //delay(500);
+    digitalWrite(PumpLine2, HIGH);
+    //delay(500);
+    digitalWrite(PumpLine3, HIGH);
+    //delay(500);
+    digitalWrite(PumpLine4, HIGH);
+    //delay(500);
+    pumpPrevMillis = millis();
+  }
+
+  if(serialRead == 'b')
+  {
+    Blink();
+    digitalWrite(PumpLine1, LOW);
+    //delay(500);
+    digitalWrite(PumpLine2, LOW);
+    //delay(500);
+    digitalWrite(PumpLine3, HIGH);
+    //delay(500);
+    digitalWrite(PumpLine4, HIGH);
+    //delay(500);
+    pumpPrevMillis = millis();
+  }
+
+  if(serialRead == 'c')
+  {
+    Blink();
+    digitalWrite(PumpLine1, LOW);
+   // delay(500);
+    digitalWrite(PumpLine2, LOW);
+    //delay(500);
+    digitalWrite(PumpLine3, LOW);
+    //delay(500);
+    digitalWrite(PumpLine4, HIGH);
+   // delay(500);
+    pumpPrevMillis = millis();
+  }
+
+  if(serialRead == 'd')
+  {
+    Blink();
+    digitalWrite(PumpLine1, LOW);
+    //delay(500);
+    digitalWrite(PumpLine2, LOW);
+    //delay(500);
+    digitalWrite(PumpLine3, LOW);
+    //delay(500);
+    digitalWrite(PumpLine4, LOW);
+   // delay(500);
+    pumpPrevMillis = millis();
+  }
+  
+  if(serialRead == 's')
+  {
+    digitalWrite(LED, LOW);
+    digitalWrite(Buzzer, LOW);
+    pumpCurrMillis = millis();
+    if ((unsigned long)(pumpCurrMillis - pumpPrevMillis) >= pumpInterval)
+    {
+      pumpCurrMillis = pumpPrevMillis;
+      digitalWrite(PumpLine1, HIGH);
+     // delay(500);
+      digitalWrite(PumpLine2, HIGH);
+      //delay(500);
+      digitalWrite(PumpLine3, HIGH);
+    //  delay(500);
+      digitalWrite(PumpLine4, HIGH);
+     // delay(500);
+    }
+    delay(20);
+  }
+}
+
+void Blink()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    if (ledState == LOW)
+    {
+      ledState = HIGH;
+    }
+    else
+    {
+      ledState = LOW;
+    }
+    digitalWrite(LED, ledState);
+    digitalWrite(Buzzer, ledState);
+  }  
+}
+
+
